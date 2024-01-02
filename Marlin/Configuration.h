77ab2e8cb90b02248332b719bca6883106dc5f44
Dan Royer <dan@marginallyclever.com>
2024-01-02 09:38:52 -0800

    update name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Marginally Clever, Sixi3 config)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Marginally Clever, Sixi3)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -153,7 +153,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Makelangelo 5"
+#define CUSTOM_MACHINE_NAME "Sixi 3 robot arm"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
